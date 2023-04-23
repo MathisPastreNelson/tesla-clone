1ac1acc7ca59ee856ee638ecd4a2b0ec0cb264f8
MathisPastreNelson <shiray@hotmail.fr>
2023-04-23 17:03:54 +0200

    header rendu ok intégration img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import logo from "../assets/tesla-logo.png";
 
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <img src={logo} className="logoHeader" alt="logo" />
+      <div className="textHeader_box">
+        <div className="productHeader_box">
+          <a href="">Model S</a>
+          <a href="">Model 3</a>
+          <a href="">Model X</a>
+          <a href="">Model Y</a>
+          <a href="">Powerwall</a>
+          <a href="">Recharge</a>
+        </div>
+        <div className="menuHeader_box">
+          <a href="">Assistance</a>
+          <a href="">Shop</a>
+          <a href="">Compte</a>
+          <a href="">Menu</a>
+        </div>
+      </div>
+    </header>
+  );
 }
