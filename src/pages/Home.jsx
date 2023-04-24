85448651c1eeb8b3b6b4304de8c276c5771f30b3
MathisPastreNelson <shiray@hotmail.fr>
2023-04-25 01:46:48 +0200

    test de scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import ModelY from "../components/ModelY";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      event.preventDefault();
+      const { deltaY } = event;
+
+      if (deltaY > 0) {
+        setScrollPosition((prevPosition) => prevPosition + window.innerHeight);
+      } else {
+        setScrollPosition((prevPosition) => prevPosition - window.innerHeight);
+      }
+    };
+
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowDown") {
+        event.preventDefault();
+        setScrollPosition((prevPosition) => prevPosition + window.innerHeight);
+      } else if (event.key === "ArrowUp") {
+        event.preventDefault();
+        setScrollPosition((prevPosition) => prevPosition - window.innerHeight);
+      }
+    };
+
+    const handleTouchMove = (event) => {
+      event.preventDefault();
+      const { touches } = event;
+      const { clientY } = touches[0];
+
+      if (clientY < window.innerHeight / 2) {
+        setScrollPosition((prevPosition) => prevPosition - window.innerHeight);
+      } else {
+        setScrollPosition((prevPosition) => prevPosition + window.innerHeight);
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll, { passive: false });
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("touchmove", handleTouchMove, { passive: false });
+
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: scrollPosition, behavior: "smooth" });
+  }, [scrollPosition]);
+
   return (
     <div>
       <Header />
-      <ModelY />
-      <ModelY />
+      <div style={{ height: "100vh" }}>
+        <ModelY />
+      </div>
+      <div style={{ height: "100vh" }}>
+        <ModelY />
+      </div>
+      <div style={{ height: "100vh" }}>
+        <ModelY />
+      </div>
       <Footer />
     </div>
   );
